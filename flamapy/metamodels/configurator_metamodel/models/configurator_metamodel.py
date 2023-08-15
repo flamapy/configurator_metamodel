00d1eef3a21f13f3094e068da0991e0c407a3df8
José A <malawito@gmail.com>
2023-08-15 19:26:25 +0200

    initial commit with basic structure

---
+++ flamapy/metamodels/configurator_metamodel/models/configurator_metamodel.py
@@ -0,0 +1,17 @@
+
+class ConfiguratorModel(VariabilityModel):
+
+    @staticmethod
+    def get_extension() -> str:
+        return 'configurator_metamodel'
+
+    def __init__(self) -> None:
+        feature_model
+        questions=[question]]
+
+class Question():
+    def __init__(self) -> None:
+        title = Feature
+        options = [Features]
+
+#----------------------
---
+++ flamapy/metamodels/configurator_metamodel/transformation/fm_to_configurator.py
@@ -0,0 +1,78 @@
+
+
+class FmToConfigurator(ModelToModel):
+    @staticmethod
+    def get_source_extension() -> str:
+        return 'fm'
+
+    @staticmethod
+    def get_destination_extension() -> str:
+        return 'configurator_metamodel'
+
+    def __init__(self, source_model: FeatureModel) -> None:
+        self.source_model = source_model
+        self.counter = 1
+        self.destination_model = ConfiguratorModel()
+    
+    def transform(self) -> ConfiguratorModel:
+        for feeature in _inorder_traversal(self.source_model.root)
+            if feature.hasChilds():
+                question = Question(feature.name)
+                for child in feature.childs:
+                    option = Option(child.name)
+                self.destination_model.add_question(question)
+                
+        return self.destination_model
+
+  
+    def _inorder_traversal(self, feature=None, result=None):
+        if result is None:
+            result = []
+        if feature is None:
+            feature = self.root
+        if feature is not None:
+            for i, child in enumerate(feature.relations):
+                if i == 0:
+                    self.inorder_traversal(child, result)
+                result.append(feature)  # Add the root (or 'parent' in this context)
+                if i != 0:
+                    self.inorder_traversal(child, result)
+        return result
+
+    def _postorder_traversal(self, feature=None, result=None):
+        if result is None:
+            result = []
+        if feature is None:
+            feature = self.root
+        if feature is not None:
+            for child in feature.relations:
+                self.postorder_traversal(child, result)
+            result.append(feature)  # Add the root last
+        return result
+
+    def _preorder_traversal_iterative(self, feature=None):
+        if feature is None:
+            feature = self.root
+        result = []
+        stack = [feature]
+        while stack:
+            current = stack.pop()
+            if current:
+                result.append(current)
+                for child in reversed(current.relations):  # reversed because we want to visit leftmost child first
+                    stack.append(child)
+        return result
+
+    def _custom_order(self):
+        print("Enter the names of features in the order you want them to be processed:")
+        custom_order = []
+        while True:
+            feature_name = input("Enter a feature name (or 'done' to finish): ")
+            if feature_name.lower() == 'done':
+                break
+            feature = self.get_feature_by_name(feature_name)
+            if feature is not None:
+                custom_order.append(feature)
+            else:
+                print(f"No feature found with name '{feature_name}', please try again.")
+        return custom_order
